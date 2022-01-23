e79378c9ca2fcd080a8f6a3f7c25db0f306da6ca
Gil <elbazg123@gmail.com>
2022-01-23 15:38:30 +0200

    fixing bbc extractor

--- NewsExtractor.py
+++ NewsExtractor.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 URL = 'https://www.bbc.com/'
-armadilo = 'https://www.bbc.com/news/uk-60083200'
 
 
 class NewsExtractor(BaseExtractor):
@@ -26,48 +25,51 @@ class NewsExtractor(BaseExtractor):
     5) ideas
     '''
 
+    @staticmethod
+    def split_url_list(url_list):
+        '''
+        :param url_list: a list of all the hrefs from the main BBC website
+        :return: two lists, one with /news and all the other types such as future, art etc.
+        '''
+        news_only = [url for url in url_list if 'news' in url]
+        others = [url for url in url_list if 'news' not in url]
+
+        return news_only, others
+
     def get_data(self):
         ignore = ['sport', 'ideas', 'in-pictures']
         self.get(URL)
-        bbc_hrefs = []
-
-        '''
-        self.get(armadilo)
-        header = self.get_element_text_by_xpath("//h1")
-        print(header)
-        content = self.get_elements_text_by_xpath("//div/p")
-        print(content)
-
         self.get_bbc_urls()
         time.sleep(1)
-        media_list = self.find_elements_by_css_selector("a.block-link__overlay-link")
+        url_list = self.find_elements_by_css_selector("a.block-link__overlay-link")
 
-        for link in media_list:
-            bbc_hrefs.append(link.get_attribute('href'))
-            print(link.get_attribute('href'))
+        hrefs = [link.get_attribute('href') for link in url_list]
+        news, others = self.split_url_list(hrefs)
+        print(news)
+        print(others)
 
-        for url in bbc_hrefs:
+        for url in hrefs:
             # if ignore not in url and 'news' in url:
             if any(domain in url for domain in ignore):
                 print(url)
                 continue
             else:
                 self.get(url)
-                header = self.get_element_text_by_id("main-heading")
-                data_block = self.get_elements_presence_by_xpath("//div[@data-component='text-block']")
+                header = self.get_element_text_by_id("//*[@id='main-heading']")
+                data_block = self.get_elements_presence_by_xpath("//*[@id='main-content']//article/div//p")
                 print(header)
                 print(data_block)
                 self.previous_page()
             '''
         self.get_bbc_urls()
         time.sleep(1)
-        media_list = self.find_elements_by_css_selector("a.block-link__overlay-link")
+        url_list = self.find_elements_by_css_selector("a.block-link__overlay-link")
 
-        for link in media_list:
-            bbc_hrefs.append(link.get_attribute('href'))
+        for link in url_list:
+            all_urls.append(link.get_attribute('href'))
             print(link.get_attribute('href'))
 
-        for url in bbc_hrefs:
+        for url in all_urls:
             if 'sport' not in url and 'news' not in url:
                 self.get(url)
                 header = self.get_element_text_by_xpath("//div[@tabindex='-1']")
@@ -79,6 +81,7 @@ class NewsExtractor(BaseExtractor):
 
     def search(self, expression, file_type='.txt'):
         self.search(expression, file_type)
+        '''
 
 
 def main():
